01d6fceae681043f5a43241e18547c0a633d0524
1 <nzgr1@mail.ru>
2020-04-21 00:05:42 +0300

    .

--- .idea/test/testt.java
+++ .idea/test/testt.java
@@ -15,12 +15,7 @@ public class testt {
     public void test1() {
         Utilita grep = new Utilita(true, false, false);
         ArrayList<String> rez = new ArrayList<String>();
-        rez.add("Импорт рабочей области Eclipse");
-        rez.add("При работе с проектами Eclipse можно использовать мастер импорта проектов Eclipse");
-        rez.add("для создания проектов NetBeans из проектов рабочей области Eclipse и импорта путей к классам и других");
-        rez.add("параметров проекта. При использовании мастера импорта проектов Eclipse создание и настройка проекта NetBeans в");
-        rez.add("ручную не требуется. Откройте мастер импорта, выбрав \"Файл\" > \"Импортировать проект\" > \"Проект Eclipse\".");
-        rez.add("Eclipse и NetBeans см. в разделе Импорт проектов Eclipse в IDE NetBeans.");
-        assertEquals(rez, grep.grep("Eclipse", "C:\\Users\\1\\IdeaProjects\\untitled3\\.idea\\test\\1.txt"));
+        rez.add("spying on the neighbours. The Dursleys had a small son called Dudley");
+        assertEquals(rez, grep.grep("Dudley", "C:\\Users\\1\\IdeaProjects\\untitled3\\.idea\\test\\1.txt"));
     }
 }
